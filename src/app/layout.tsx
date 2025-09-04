2a4b24cc7e6149fa67912c69576fffbcca3c04f5
lakshyakumar90 <lakshyakumar5023@gmail.com>
2025-09-04 20:37:49 +0530

    init commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        helllllllloooooooo
         {children}
       </body>
     </html>
