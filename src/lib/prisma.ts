1e27a9d841ae01a695297cf5eeb26d4ab8a5dde9
lakshyakumar90 <lakshyakumar5023@gmail.com>
2025-12-08 19:39:29 +0530

    build error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,22 @@
+// src/lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
 export const prisma =
-  global.prisma ||
+  global.prisma ??
   new PrismaClient({
+    adapter,
     log: ["error", "warn"],
   });
 
 if (process.env.NODE_ENV !== "production") {
   global.prisma = prisma;
 }
-
-
